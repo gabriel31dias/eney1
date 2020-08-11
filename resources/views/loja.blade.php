3f1d788f3e81b1094378f9c5d0c9df971e3a996d
Your Name <you@example.com>
2020-08-11 09:56:53 -0300

    'wsss'

--- resources/views/loja.blade.php
+++ resources/views/loja.blade.php
@@ -499,7 +499,7 @@ async function open_product(id,nomeproduto,preco,descr,img){
 	</div>
 
 	<div class="col-xs-6 col-lg-6 col-md-6 col-sm-6">
-		<button type="button"  onclick="add_adicionais(${id})" >  <i style="margin-top:3px;"  class="large material-icons">local_mall</i> ADICIONAIS</button>
+		<button type="button"  onclick="add_adicionais(${id})" >  <i style="margin-top:3px;"  class="large material-icons">local_mall</i>ADICIONAIS</button>
 	</div>
 
 	</center>
