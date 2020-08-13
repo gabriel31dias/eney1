d788691437be50e4dcfd63c13e6c4a9a3cf19e2c
Your Name <you@example.com>
2020-08-13 08:40:02 -0300

    'wsss'

--- resources/views/loja.blade.php
+++ resources/views/loja.blade.php
@@ -584,7 +584,7 @@ if (formValues) {
 	 document.getElementById('quantidade').value =   cont_quantidade
      alert(preco_old)
 	 cach_produto[2] = (preco_old + total_adicionais) * cont_quantidade
-	 cach_produto[2] =   cach_produto[2].toFixed(2)
+	
 
 	 //cach_produto[2] =  cach_produto[2] * cont_quantidade
 	 //cach_produto[2] = parseFloat(cach_produto[2]).toFixed(2)
@@ -600,7 +600,7 @@ if (formValues) {
 	  cont_quantidade = cont_quantidade - 1
 	  document.getElementById('quantidade').value =  cont_quantidade
 	  cach_produto[2] = (preco_old + total_adicionais) * cont_quantidade
-	  cach_produto[2] =   cach_produto[2].toFixed(2)
+	 
     }
 
 
