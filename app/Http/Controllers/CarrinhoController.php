831bb91fe21147cd52e1ac9e2224a213332e832f
Your Name <you@example.com>
2020-08-18 10:04:42 -0300

    'wss'

--- app/Http/Controllers/CarrinhoController.php
+++ app/Http/Controllers/CarrinhoController.php
@@ -43,7 +43,7 @@ class CarrinhoController extends Controller
 
     public function index($codigo = '')
     {
-        $produtosjson = json_encode(Session::get('carrinho'));
+        $produtosjson = json_encode($this->removefotosall(Session::get('carrinho')));
         $iduser = DB::table('users')->where('codigo_estabelecimento', $codigo)->first();
         $iduser = $iduser->id;
         $teste = Session::get('idloja');
