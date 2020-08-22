87c1f43f517de926c543f644d39c78be7ab28340
Your Name <you@example.com>
2020-08-22 10:11:12 -0300

    'sss

--- database/migrations/2020_07_20_124859_create_adicionados_table.php
+++ database/migrations/2020_07_20_124859_create_adicionados_table.php
@@ -15,6 +15,7 @@ class CreateAdicionadosTable extends Migration
     {
         Schema::create('adicionados', function (Blueprint $table) {
             $table->id();
+            $table->integer('CODIGO_SISTEMA');
             $table->integer('ID_ADICIONAL')->nullable();
             $table->integer('ID_PRODUTO')->nullable();
             $table->string('DESCR_ADICIONAL')->nullable();
