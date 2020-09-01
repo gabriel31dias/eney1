9e05d7b67115310907811f9d444b8676cb2f017d
Your Name <you@example.com>
2020-09-01 08:11:39 -0300

    'dd

---
+++ app/Http/Controllers/CieloCheckoutlink.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class CieloCheckoutlink extends Controller
+{
+
+    private $Mercantid;
+
+    
+    
+
+    public function addMerchantId($mercantid){
+      $this->Mercantid = $mercantid;
+    }
+
+    public function executa(){
+
+        $curlx = curl_init();
+        curl_setopt_array($curlx, array(
+           CURLOPT_URL => "https://cieloecommerce.cielo.com.br/api/public/v1/orders",
+           CURLOPT_RETURNTRANSFER => true,
+           CURLOPT_ENCODING => "",
+           CURLOPT_MAXREDIRS => 10,
+           CURLOPT_TIMEOUT => 0,
+           CURLOPT_FOLLOWLOCATION => true,
+           CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+           CURLOPT_CUSTOMREQUEST => "POST",
+           CURLOPT_POSTFIELDS =>"{  \n   \"OrderNumber\":\"Pedido013232\",\n   \"SoftDescriptor\":\"Exemplo\",\n   \"Cart\":{  \n      \"Discount\":{  \n         \"Type\":\"Percent\",\n         \"Value\":00\n      },\n      \"Items\":[  \n         {  \n            \"Name\":\"wohax sistema\",\n            \"Description\":\"sistema xxx\",\n            \"UnitPrice\":100,\n            \"Quantity\":1,\n            \"Type\":\"Asset\",\n            \"Sku\":\"ABC001\",\n            \"Weight\":500\n         },\n        ]\n   },\n   \"Shipping\":{  \n      \"SourceZipCode\":\"20020080\",\n      \"TargetZipCode\":\"21911130\",\n      \"Type\":\"FixedAmount\",\n      \"Services\":[  \n         {  \n            \"Name\":\"Motoboy\",\n            \"Price\":1,\n            \"Deadline\":15,\n            \"Carrier\":null\n         },\n         {  \n            \"Name\":\"UPS Express\",\n            \"Price\":1,\n            \"Deadline\":2,\n            \"Carrier\":null\n         }\n      ],\n      \"Address\":{  \n         \"Street\":\"Rua Cambui\",\n         \"Number\":\"92\",\n         \"Complement\":\"Apto 201\",\n         \"District\":\"Freguesia\",\n         \"City\":\"Rio de Janeiro\",\n         \"State\":\"RJ\"\n      }\n   },\n   \"Payment\":{  \n      \"BoletoDiscount\":15,\n      \"DebitDiscount\":10,\n      \"Installments\":null,\n      \"MaxNumberOfInstallments\": null\n   },\n   \"Customer\":{  \n      \"Identity\":\"84261300206\",\n      \"FullName\":\"Test de Test\",\n      \"Email\":\"test@cielo.com.br\",\n      \"Phone\":\"21987654321\"\n   },\n   \"Options\":{  \n     \"AntifraudEnabled\":true,\n     \"ReturnUrl\": \"http://www.cielo.com.br\"\n   },\n   \"Settings\":null\n}\n",
+           CURLOPT_HTTPHEADER => array(
+        "MerchantId: 35c778b2-f9b1-478c-bc7a-2667f6027652",
+        "Content-type: application/json",
+        "Cookie: ARRAffinity=24b69ec43a385a35f00243c885d57b9207d8ca54474a7de7492216dc4f81280d"
+         ),
+         ));
+       $response = curl_exec($curlx);
+       curl_close($curlx);
+       echo $response;
+  
+    }
+
+    
+
+    public function addMerchantId(){
+      
+        
+
+    }
+
+
+    public function addMerchantId(){
+      
+        
+
+    }
+    
+    
+ 
+
+
+
+
+
+
+}
--- resources/views/loja.blade.php
+++ resources/views/loja.blade.php
@@ -642,6 +642,8 @@ async function adicionar_carrinho(id,nomeproduto){
 			return
 		}
 
+
+
       
 
         $.ajax({
@@ -660,10 +662,43 @@ async function adicionar_carrinho(id,nomeproduto){
             data: JSON.stringify(produto)
         })
 		
+
 		arrayadd = []
 
 		$( '.mfp-close' ).click();
 		swalWithBootstrapButtons.close()
+
+
+		
+let timerInterval
+await Swal.fire({
+  title: 'Auto close alert!',
+  html: 'I will close in <b></b> milliseconds.',
+  timer: 2000,
+  timerProgressBar: true,
+  onBeforeOpen: () => {
+    Swal.showLoading()
+    timerInterval = setInterval(() => {
+      const content = Swal.getContent()
+      if (content) {
+        const b = content.querySelector('b')
+        if (b) {
+          b.textContent = Swal.getTimerLeft()
+        }
+      }
+    }, 100)
+  },
+  onClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
+
+
 
 Swal.fire(
   'Adicionado ao carrinho !',
