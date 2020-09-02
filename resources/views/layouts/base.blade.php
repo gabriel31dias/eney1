92743c4c09ed23eaff7c00ff2f669a7a8f6e3ce2
Your Name <you@example.com>
2020-09-02 14:24:39 -0300

    '1dd

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -742,7 +742,7 @@ select {
 var contnf = 0
 async function notificacao(venda){
    contnf = contnf + 1
-   $("#ppp").val(contnf)
+   $("#ppp").html(contnf)
     let lihtml = `
                               <li>
                                  <a href="javascript:void(0);">
