519be969168e068694dbdcc268422313fe1acae3
Your Name <you@example.com>
2020-09-02 11:39:51 -0300

    'ddd

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -740,7 +740,7 @@ select {
 
 async function notificacao(venda){
     alert("insertado")
-    let lihtml = "<li>${venda.nome}</li>"
+    let lihtml = `<li>${venda.nome}</li>`
     $("#ulnotifica").append(lihtml)
 }
 
