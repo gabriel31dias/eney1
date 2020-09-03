31622314268925a583bd953a559af13d6bf351c2
Your Name <you@example.com>
2020-09-03 09:15:12 -0300

    'dswda

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -321,6 +321,14 @@ select {
             </div>
             <div class="collapse navbar-collapse" id="navbar-collapse">
                <ul class="nav navbar-nav navbar-right">
+                  <li class="dropdown">
+                     <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="true">
+                     <i class="material-icons">visibility</i>
+                     </a>
+                    
+                  </li>
+
+
                   <!-- Call Search -->
                   <li><a href="javascript:void(0);" class="js-search" data-close="true"><i class="material-icons">search</i></a></li>
                   <!-- #END# Call Search -->
