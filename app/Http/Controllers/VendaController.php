54aeebf8a11593dffa06fd06ef790e68bc5a6bbf
Your Name <you@example.com>
2020-09-12 09:10:16 -0300

    'dwda

--- app/Http/Controllers/VendaController.php
+++ app/Http/Controllers/VendaController.php
@@ -409,7 +409,7 @@ class VendaController extends Controller
 
 
     public function cielopagamento(Request $req){
-        return response()->json($req->produtos);
+        return response()->json($req);
      
         $cielo = new CieloCheckoutlink();
         $cielo = $cielo->addMerchantId('35c778b2-f9b1-478c-bc7a-2667f6027652');
