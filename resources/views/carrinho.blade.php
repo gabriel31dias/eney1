4a2dc17beefe603ece85c2ed69d0ab5fb93d31b8
Your Name <you@example.com>
2020-09-14 17:22:27 -0300

    'dwada

--- resources/views/carrinho.blade.php
+++ resources/views/carrinho.blade.php
@@ -731,8 +731,7 @@ const { value: formValues } = await temaapp.fire({
 
      <div class="form-group col-xs-12  col-md-12">
       <label for="inputZip">Digite seu Email</label>
-	  <input onfocus="masctel(this)" type="text" value="{{$userapp['celular'] ?? ''}}
-      " class="form-control"  value="" id="email" name="email" placeholder="" >
+	  <input onfocus="masctel(this)" type="text" value="{{$userapp['email'] ?? ''}}" class="form-control"  value="" id="email" name="email" placeholder="" >
 
     </div>
 
