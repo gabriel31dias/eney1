4ac95eb708fbacc97f6afd5f287e35d04a530420
Your Name <you@example.com>
2020-09-14 10:51:26 -0300

    'dwead]

--- resources/views/carrinho.blade.php
+++ resources/views/carrinho.blade.php
@@ -1399,7 +1399,7 @@ function atualiza_obj_user(){
             dataType: 'json',
             contentType: 'application/json',
             success: function (data) {
-				      
+				      alert(JSON.stringify(data))
               location.href=data
             },
 
