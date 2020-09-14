a1fcb19223c0157656b29ed77b74437be3d4f1ac
Your Name <you@example.com>
2020-09-14 14:59:16 -0300

    'dwead]

--- app/Http/Controllers/CieloCheckoutlink.php
+++ app/Http/Controllers/CieloCheckoutlink.php
@@ -48,7 +48,9 @@ class CieloCheckoutlink extends Controller
        $response = curl_exec($curlx);
        curl_close($curlx);
        $response = json_decode($response);
-       return $response->settings->checkoutUrl;
+     ///  return $response->settings->checkoutUrl;
+
+       return $response->settings;
     }
 
     public function AddProductList($products){
