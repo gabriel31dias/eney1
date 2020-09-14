8a62915c075c18c8749291762b3467154d190448
Your Name <you@example.com>
2020-09-14 17:35:45 -0300

    'dwad

--- resources/views/carrinho.blade.php
+++ resources/views/carrinho.blade.php
@@ -1362,7 +1362,7 @@ function atualiza_obj_user(){
         let precoformatado = i.precoproduto
         precoformatado =  precoformatado.toFixed(2);
       //  alert(precoformatado)
-         add_produto_array(i.nomeproduto, i.nomeproduto , i.precoproduto  , i.quantidade , "Asset", "ABC001" , 100)
+         add_produto_array(i.nomeproduto + i.tags_adicionais , i.nomeproduto , i.precoproduto  , i.quantidade , "Asset", "ABC001" , 100)
          
       }
 
@@ -1374,7 +1374,7 @@ function atualiza_obj_user(){
 
     async function add_produto_array(nomeproduto, descricao , unitpreco , qtd , type="Asset", Sku="ABC001" , weight=100){
       obj_produtos = {
-         Name:nomeproduto,
+         Name:nomeproduto ,
          Description:descricao,
          UnitPrice: unitpreco * 100 ,
          Quantity:qtd,
