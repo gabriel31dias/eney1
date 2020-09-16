2109b1f82f7d5bebb712625cc59abce178ad4cb2
Your Name <you@example.com>
2020-09-16 15:13:48 -0300

    '1ef

--- app/Http/Controllers/VendaController.php
+++ app/Http/Controllers/VendaController.php
@@ -481,7 +481,7 @@ class VendaController extends Controller
 
     public function TesteHttpSocket(){
 
-        $response = Http::post('http://localhost:3400/comunica', [
+        $response = Http::post('https://servidorsocket3636.herokuapp.com/comunica', [
             'name' => 'Steve',
             'role' => 'Network Administrator',
           ]);
