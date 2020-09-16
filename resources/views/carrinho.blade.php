4b3fbdbcec3b49dc902440123dccb84d38e42e40
Your Name <you@example.com>
2020-09-16 09:36:47 -0300

    'dwa

--- resources/views/carrinho.blade.php
+++ resources/views/carrinho.blade.php
@@ -1459,8 +1459,8 @@ function IsEmail(email){
 
  async function screen_concluida(){
   await  Swal.fire(
-    'Muito bom, A venda foi concluida, com sucesso, mande um Oi no nosso whats para qualquer duvida! <br> <a href="https://api.whatsapp.com/send?phone={{$whats_contato ?? ''}}&text=Ol%C3%A1%20estou%20aguardando%20o%20pedido.">Abrir Whats</a>',
-    '',
+    'Muito bom, A venda foi concluida, com sucesso, mande um Oi no nosso whats para qualquer duvida! ',
+    '<br> <a href="https://api.whatsapp.com/send?phone={{$whats_contato ?? ''}}&text=Ol%C3%A1%20estou%20aguardando%20o%20pedido.">Abrir Whats</a>',
     'success'
     )
     
