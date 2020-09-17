541b08a729b87f27f23f48f9c57317144a70b539
Your Name <you@example.com>
2020-09-17 17:09:27 -0300

    'dwa

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,13 +14,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment() !== 'production') {
+       // if ($this->app->environment() !== 'production') {
             
-        }  
+      //  }  
         
-        if (app()->environment('remote')) {
+       // if (app()->environment('remote')) {
             URL::forceSchema('https');
-        }
+       // }
     }
 
     /**
