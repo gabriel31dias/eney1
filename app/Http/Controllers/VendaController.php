8ec8fcd4f306b097a099672153cecc023bc84705
Your Name <you@example.com>
2020-09-17 09:32:06 -0300

    'dwa

--- app/Http/Controllers/VendaController.php
+++ app/Http/Controllers/VendaController.php
@@ -484,7 +484,7 @@ class VendaController extends Controller
 
     public function TesteHttpSocket(){
         $client = new SocketIOClient('https://servidorsocket3636.herokuapp.com/');
-       $client->connect($client);
+       $client->connect();
         //send messag
       $client->message('fefe');
         //emit event
