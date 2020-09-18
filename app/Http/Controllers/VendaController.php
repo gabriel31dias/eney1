14aa11b1dbb51a60968b52aacf9382fddfca1101
Your Name <you@example.com>
2020-09-18 17:31:06 -0300

    'dwead

--- app/Http/Controllers/VendaController.php
+++ app/Http/Controllers/VendaController.php
@@ -558,10 +558,10 @@ class VendaController extends Controller
 
         foreach ($vendasnaoenviadas as $value) {
 
-          if(! array_search($value->cod_venda_web ,$array_loja_enviada ,false) ){
+         // if(! array_search($value->cod_venda_web ,$array_loja_enviada ,false) ){
               $this->socketEmitVenda($value->venda_json);
               echo $value->id;
-          }
+         // }
          
 
           
