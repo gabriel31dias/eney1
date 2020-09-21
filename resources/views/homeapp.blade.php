eaf40279f8d2cd1ccdcbdbf405bb83123f9141ac
Your Name <you@example.com>
2020-09-21 14:33:38 -0300

    'dswda

--- resources/views/homeapp.blade.php
+++ resources/views/homeapp.blade.php
@@ -136,6 +136,58 @@ visibility: hidden;
 </div>
 
 
+
+
+<div class="row clearfix">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <h2>
+                    VENDAS NÃO APROVADAS 
+                </h2>
+                <ul class="header-dropdown m-r--5">
+                    <li class="dropdown">
+                        <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            <i class="material-icons">more_vert</i>
+                        </a>
+                        <ul class="dropdown-menu pull-right">
+                            <li><a href="javascript:void(0);" class=" waves-effect waves-block">Action</a></li>
+                            <li><a href="javascript:void(0);" class=" waves-effect waves-block">Another action</a></li>
+                            <li><a href="javascript:void(0);" class=" waves-effect waves-block">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="body table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>NOME</th>
+                            <th>TOTAL</th>
+                            <th>HORA</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($vendasnaoaprovadas as $item)
+
+                        <tr>
+                            <td>{{$item->nomecliente}}</td>
+                            <td>{{$item->preco_total_produto + $item->preco_total_entrega }}</td>
+                            <td>{{$item->created_at}}</td>
+                            <td><a href="https://api.whatsapp.com/send?phone=55{{$item->numerotelefone}}&text=Ola!" type="button" class="btn bg-green btn-circle waves-effect waves-circle waves-float">
+                                <i class="material-icons">chat</i>
+                            </a></td>
+                        </tr>
+                            
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 </div>
 </div>
 </section>
