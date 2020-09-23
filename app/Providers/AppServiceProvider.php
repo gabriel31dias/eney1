de85d79eb37a440de2b3a4aeb11e4f948ed3ca36
Your Name <you@example.com>
2020-09-23 16:12:28 -0300

    'swqwdaa

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,9 +18,9 @@ class AppServiceProvider extends ServiceProvider
             
        // }  
 
-       URL::forceScheme('http');
+      // URL::forceScheme('http');
         
-      /// URL::forceScheme('https');
+      URL::forceScheme('https');
     }
 
     /**
