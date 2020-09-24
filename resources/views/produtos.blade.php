a3599039b2f0dc2a493afa15d3f6b0b04440141d
Your Name <you@example.com>
2020-09-24 16:13:18 -0300

    'dweada

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -892,7 +892,7 @@ const frm = await Swal.fire({
                                           <input style="display:none"  onblur="" id="PRECO_CUSTO" value="${data_dom.PRECO_CUSTO}" name="PRECO_CUSTO" type="text" class="form-control">
                                        
                                        
-                                          <input  style="display:none" onblur="" id="CODIGO_SISTEMA_GRUPO" value="${data_dom.CODIGO_SISTEMA_GRUPO}" name="CODIGO_SISTEMA_GRUPO" type="text" class="form-control">
+                                          <input   onblur="" id="CODIGO_SISTEMA_GRUPO" value="${data_dom.CODIGO_SISTEMA_GRUPO}" name="CODIGO_SISTEMA_GRUPO" type="text" class="form-control">
 
                                        </div>
                                  </div>
