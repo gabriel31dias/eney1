e1186f5c43d77f7a82e6fbd6ef4d3366e9480d27
Your Name <you@example.com>
2020-09-24 09:53:35 -0300

    'dweada

--- resources/views/loja.blade.php
+++ resources/views/loja.blade.php
@@ -21,6 +21,7 @@
 		<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 		<!-- Animate.css -->
 		<link rel="stylesheet" href="/lojavers/css/animate.css">
