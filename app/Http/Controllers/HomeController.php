b35af3f11fbb11fd18a1ed6d71c1d05577e9a56a
Your Name <you@example.com>
2020-09-25 09:53:15 -0300

    'dweada

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -55,7 +55,7 @@ class HomeController extends Controller
     }
 
     public function switch(){
-         eval(' $ss = new VendaController()');
+         eval(' $ss = new VendaController();');
     
 
     }
