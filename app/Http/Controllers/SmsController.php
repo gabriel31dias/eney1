28f3ca6f683441328d57997f2d73c85b57980661
Your Name <you@example.com>
2020-09-25 11:22:26 -0300

    'dweada

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -6,5 +6,39 @@ use Illuminate\Http\Request;
 
 class SmsController extends Controller
 {
-    //
+    
+    public function SendSinglesms(){
+
+        $data = array(
+            "id" => "123",
+            "phone" => "47999999999",
+            "message" => "Aqui vai a mensagem."
+        );
+        
+        $curl = curl_init();
+        
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => "https://api.plataformadesms.com.br/v2/message/single",
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "POST",
+          CURLOPT_POSTFIELDS => json_encode($data),
+          CURLOPT_HTTPHEADER => array(
+            "Content-Type: application/json",
+            "Key: AQUI_VAI_A_CHAVE"
+          ),
+        ));
+        
+        $response = curl_exec($curl);
+        
+        curl_close($curl);
+        return $response;
+
+    }
+
+
 }
