6ed21d46a963353f7065b5918f5fc831a87d6089
Your Name <you@example.com>
2020-09-25 12:58:04 -0300

    'dewdaw

--- app/Http/Controllers/SmsValidController.php
+++ app/Http/Controllers/SmsValidController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\SmsController;
+use App\Validatestoken;
 
 class SmsValidController extends Controller
 {
     
     public function sendSmsToken($telefone){
       $sms = new SmsController();
-
-      return   $sms->sendSinglesms('0030015529',$telefone,'dwadaw token dwadwadadawdw5555');
+      $generatetoken = rand(100,1000);
+      $newtoken = new Validatestoken();
+      $newtoken->token = $generatetoken;
+      $newtoken->save();
+      return   $sms->sendSinglesms('0030015529',$telefone,'O seu token do WebApp é'.$generatetoken );
    }
 }
