e26a38bb41d8b1b51ae7600b0a602cf5433502b3
Your Name <you@example.com>
2020-09-25 16:52:13 -0300

    'dwad

--- app/Http/Controllers/SmsValidController.php
+++ app/Http/Controllers/SmsValidController.php
@@ -16,7 +16,7 @@ class SmsValidController extends Controller
       $newtoken->token = $generatetoken;
       $newtoken->save();
       //return   $sms->sendSinglesms('0030015529',$telefone,'O seu token do WebApp é'.$generatetoken );
-      return    $newtoken->all();
+      return  response()->json($newtoken->all()) ;
    }
 
    public function verificatoken($token){
