7d0c0c2b11bd363dee60a5298a104671a0451891
Your Name <you@example.com>
2020-09-28 17:31:45 -0300

    'dwada

--- app/Http/Controllers/SwitchForma.php
+++ app/Http/Controllers/SwitchForma.php
@@ -36,7 +36,7 @@ class SwitchForma extends Controller
    }
 
    public static function getcodeloja($req){
-      $tt =  $this->temp;
+    //  $tt =  $this->temp;
       $auxvend = explode("-", $req->order_number);
 
       return  $auxvend[0];
