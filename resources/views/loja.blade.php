800fdad8a9cf03b51327d0ffc208ed25ae55e4c1
Your Name <you@example.com>
2020-10-01 10:44:45 -0300

    'dwad

--- resources/views/loja.blade.php
+++ resources/views/loja.blade.php
@@ -282,43 +282,7 @@
 			
 			
 		<br>		
-<div class="container fh5co-card-item"  style="height: 400px;">
-	<h2 style="color:#FBB448">Promoções</h2>  
-	<div id="myCarousel" class="carousel slide" data-ride="carousel">
-	  <!-- Indicators -->
-	  <ol class="carousel-indicators">
-		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		<li data-target="#myCarousel" data-slide-to="1"></li>
-		<li data-target="#myCarousel" data-slide-to="2"></li>
-	  </ol>
 
-	  @isset($promocões)
-	  <div class="carousel-inner">
-	  @foreach ($promocões as $item)
-  
-	  <!-- Wrapper for slides -->
-	 
-		<div class="item active">
-		  <img src="https://m.bonde.com.br/img/bondenews/2019/11/img_4245.jpg" onclick="open_product('{{$item->id}}','{{$item->NOME_PRODUTO}}','{{$item->PRECO_UNIT}}','{{$item->DESCR}}','{{$item->IMG}}');	preco_old = {{$item->PRECO_UNIT}}"  style="max-height: 300px;height: 250px;"  alt="Los Angeles" style="width:100%;">
-		</div>
-  
-	 
-
-	  @endforeach
-
-	</div>
-  
-	  <!-- Left and right controls -->
-	  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-		<span class="glyphicon glyphicon-chevron-left"></span>
-		<span class="sr-only">Previous</span>
-	  </a>
-	  <a class="right carousel-control" href="#myCarousel" data-slide="next">
-		<span class="glyphicon glyphicon-chevron-right"></span>
-		<span class="sr-only">Next</span>
-	  </a>
-	</div>
-  </div>
 
             
         
