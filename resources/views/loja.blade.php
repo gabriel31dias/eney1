f0c760de103b03621f2ef08eb7ce7b92a94215c1
Your Name <you@example.com>
2020-10-01 10:27:39 -0300

    'dwad

--- resources/views/loja.blade.php
+++ resources/views/loja.blade.php
@@ -295,7 +295,7 @@
 	  <!-- Wrapper for slides -->
 	  <div class="carousel-inner">
 		<div class="item active">
-		  <img src="https://m.bonde.com.br/img/bondenews/2019/11/img_4245.jpg"  style="max-height: 300px;"  alt="Los Angeles" style="width:100%;">
+		  <img src="https://m.bonde.com.br/img/bondenews/2019/11/img_4245.jpg"  style="max-height: 300px;height: 250px;"  alt="Los Angeles" style="width:100%;">
 		</div>
   
 		<div class="item">
