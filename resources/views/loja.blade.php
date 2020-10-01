9686892bf20b81826523334c0045bcf05828198f
Your Name <you@example.com>
2020-10-01 10:41:24 -0300

    'dwad

--- resources/views/loja.blade.php
+++ resources/views/loja.blade.php
@@ -291,21 +291,22 @@
 		<li data-target="#myCarousel" data-slide-to="1"></li>
 		<li data-target="#myCarousel" data-slide-to="2"></li>
 	  </ol>
+
+	  @isset($promocões)
+	  <div class="carousel-inner">
+	  @foreach ($promocões as $item)
   
 	  <!-- Wrapper for slides -->
-	  <div class="carousel-inner">
+	 
 		<div class="item active">
-		  <img src="https://m.bonde.com.br/img/bondenews/2019/11/img_4245.jpg"  style="max-height: 300px;height: 250px;"  alt="Los Angeles" style="width:100%;">
+		  <img src="https://m.bonde.com.br/img/bondenews/2019/11/img_4245.jpg" onclick="open_product('{{$item->id}}','{{$item->NOME_PRODUTO}}','{{$item->PRECO_UNIT}}','{{$item->DESCR}}','{{$item->IMG}}');	preco_old = {{$item->PRECO_UNIT}}"  style="max-height: 300px;height: 250px;"  alt="Los Angeles" style="width:100%;">
 		</div>
   
-		<div class="item">
-		  <img src="https://m.bonde.com.br/img/bondenews/2019/11/img_4245.jpg" style="max-height: 300px;" alt="Chicago" style="width:100%;">
-		</div>
-	  
-		<div class="item">
-		  <img src="ny.jpg" alt="New york" style="width:100%;">
-		</div>
-	  </div>
+	 
+
+	  @endforeach
+
+	</div>
   
 	  <!-- Left and right controls -->
 	  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
