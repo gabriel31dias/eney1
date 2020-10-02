682e7e584c6bcdc19bef34ff31b7eeee699f29b6
Your Name <you@example.com>
2020-10-02 11:15:08 -0300

    'dwad

--- resources/views/promocaoset.blade.php
+++ resources/views/promocaoset.blade.php
@@ -235,6 +235,9 @@ cash_id = id
    if (params.isDismissed) {
         
         cancelpromocao(cash_id)
+
+
+        loading()
       
         location.reload();
 
@@ -355,6 +358,39 @@ if (document.getElementById('preco').value) {
 $('.tab tr').click(function() {
       $(this).toggleClass("clicked");
 });
+
+
+async function loading(params) {
+
+   let timerInterval
+Swal.fire({
+  title: 'Auto close alert!',
+  html: 'I will close in <b></b> milliseconds.',
+  timer: 3000,
+  timerProgressBar: true,
+  willOpen: () => {
+    Swal.showLoading()
+    timerInterval = setInterval(() => {
+      const content = Swal.getContent()
+      if (content) {
+        const b = content.querySelector('b')
+        if (b) {
+          b.textContent = Swal.getTimerLeft()
+        }
+      }
+    }, 100)
+  },
+  onClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
+   
+}
 
 
 
