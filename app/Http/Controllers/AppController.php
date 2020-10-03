b1699b92368f0004047c0257c98350c4671d0225
Your Name <you@example.com>
2020-10-03 10:49:52 -0300

    'dwadaw

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -97,8 +97,8 @@ class AppController extends Controller
       $youtube = $getloja->YOUTUBE;
 
       $promoces = FacadesDB::table('produtos')->where('ID_USER', $getloja->id)->where('PROMOCAO',true)
-      ->where('DATA_INICIO_PROMOCAO', '>=' , Carbon::now() )
-      ->where('DATA_FINAL_PROMOCAO', '<=' , Carbon::now() )
+      ->where('DATA_INICIO_PROMOCAO', '>=' , Carbon::now()->date() )
+      ->where('DATA_FINAL_PROMOCAO', '<=' , Carbon::now()->date() )
       ->get();
 
    
