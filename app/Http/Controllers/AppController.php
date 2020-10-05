3b09c8f210a5e44e4a6562d20741024446eb6f94
Your Name <you@example.com>
2020-10-05 15:13:57 -0300

    'dwad

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -150,7 +150,7 @@ class AppController extends Controller
 
       $getgrupos = $this->grupos->where('ID_USER',$getidloja)->paginate(10);
     
-      return view('loja',['produtos'=>$getproducts,'style'=>$style,'grupos'=> $getgrupos,'lojacod'=>$codigo,'grupoitem'=>$grupoitem,'status_loja'=>$status_loja,'imagem_loja'=>$imagem_loja,'url_facebook'=>$facebook,'url_instagran'=>$instagram,'url_twitter'=>$twitter,'url_youtube'=>$youtube,'getsucesso'=>$getsucesso,'promoces'=>$promoces]);
+      return view('loja',['produtos'=>$getproducts,'style'=>$style,'grupos'=> $getgrupos,'lojacod'=>$codigo,'grupoitem'=>$grupoitem,'status_loja'=>$status_loja,'imagem_loja'=>$imagem_loja,'url_facebook'=>$facebook,'url_instagran'=>$instagram,'url_twitter'=>$twitter,'url_youtube'=>$youtube,'getsucesso'=>$getsucesso,'promoces'=>$promoces,'gr'=>true]);
     }
 
 
@@ -179,7 +179,7 @@ class AppController extends Controller
       $getiduserloja =    $getiduserloja->id ;
       $getprodutossearch = $this->produtos->where('ID_USER',  $getiduserloja)->where('NOME_PRODUTO', 'like', '%' . $nomeproduto . '%')->paginate(10);
       $getgrupos = $this->grupos->where('ID_USER', $getiduserloja)->paginate(10);
-      return view('loja',['produtos'=> $getprodutossearch,'style'=>$style,'grupos'=> $getgrupos,'lojacod'=>$loja,'grupoitem'=>'','status_loja'=>$status_loja,'gr'=>true]);
+      return view('loja',['produtos'=> $getprodutossearch,'style'=>$style,'grupos'=> $getgrupos,'lojacod'=>$loja,'grupoitem'=>'','status_loja'=>$status_loja]);
 
     }
 
