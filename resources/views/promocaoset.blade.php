88ebf34795527184638f1404e4bec4756f02853f
Your Name <you@example.com>
2020-10-06 09:07:21 -0300

    'dwad

--- resources/views/promocaoset.blade.php
+++ resources/views/promocaoset.blade.php
@@ -237,12 +237,12 @@ async function showset(id){
                   <div class="row">
                     <div class="col-xs-12 col-lg-6 col-md-6 col-sm-6">
                    
-                        <h3 style="backgroud-color:#96DDEA" class="lbl"> ${getini}</h3>
+                        <h3 style="backgroud-color:#96DDEA" > ${getini}</h3>
                         
                    
                     </div>
                     <div class="col-xs-12 col-lg-6 col-md-6 col-sm-6">
-                        <h3  style="backgroud-color:#96DDEA" class="lbl">${getfini}</h3>
+                        <h3  style="backgroud-color:#96DDEA" >${getfini}</h3>
                         
                     </div>
                   </div>
