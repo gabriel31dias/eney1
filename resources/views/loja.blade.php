305b26cba9ec261f6dc8fef5f937c9b230ffffa4
Your Name <you@example.com>
2020-10-06 14:48:04 -0300

    'dwad

--- resources/views/loja.blade.php
+++ resources/views/loja.blade.php
@@ -576,7 +576,7 @@ const swalWithBootstrapButtons = Swal.mixin({
 
 			 
 async function open_product(id,nomeproduto,preco,descr,img,promocao='',preco_promocao=''){
-   
+      alert(preco_promocao)
 
 	var price = preco_old ? preco_old : preco;
 	var tags_adicionais = cash_tags.toString(); //adicionais setados
