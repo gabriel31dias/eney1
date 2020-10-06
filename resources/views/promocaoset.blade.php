e24fad20e5892801de2d8879c5e680bc6639f021
Your Name <you@example.com>
2020-10-06 17:21:37 -0300

    'dwad

--- resources/views/promocaoset.blade.php
+++ resources/views/promocaoset.blade.php
@@ -501,7 +501,26 @@ Swal.fire({
 }
 
 
+async function dw(params) {
 
+   const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
+
+Toast.fire({
+  icon: 'success',
+  title: 'Signed in successfully'
+})
+   
+}
     
 
 
