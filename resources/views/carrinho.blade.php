5c82ac37d569af85be7c673d612d384fc370331c
Your Name <you@example.com>
2020-10-08 12:09:54 -0300

    'dwad

--- resources/views/carrinho.blade.php
+++ resources/views/carrinho.blade.php
@@ -1682,15 +1682,7 @@ function atualiza_obj_user(){
     }
 
 
-     
-function IsEmail(email){
-    var exclude=/[^@-.w]|^[_@.-]|[._-]{2}|[@.]{2}|(@)[^@]*1/;
-    var check=/@[w-]+./;
-    var checkend=/.[a-zA-Z]{2,3}$/;
-    if(((email.search(exclude) != -1)||(email.search(check)) == -1)||(email.search(checkend) == -1)){return false;}
-    else {return true;}
- }
- 
+
 
  async function screen_concluida(){
   await  Swal.fire(
