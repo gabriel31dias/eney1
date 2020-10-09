1165f6646699dd169d5083d61ddba221fa91f85d
Your Name <you@example.com>
2020-10-09 10:05:42 -0300

    'dwad

--- resources/views/carrinho.blade.php
+++ resources/views/carrinho.blade.php
@@ -336,7 +336,7 @@
     
                       
                         
-                        <div class="fh5co-text ">
+                        <div >
                           <center>
                             <div style="color:#FBB448" class="col-xs-12 col-lg-12 col-md-12 col-sm-12 ">
                             <h2   style="padding:10px;color:#FBB448" >{{$item['nomeproduto']}} {{$item['tags_adicionais']}}   {{ number_format($item['precoproduto'], 2) }}</h2>
