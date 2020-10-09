33c8a83189be813aa5e4eafa6a2f58a6c2f0320d
Your Name <you@example.com>
2020-10-09 08:54:16 -0300

    'dfwead

--- resources/views/carrinho.blade.php
+++ resources/views/carrinho.blade.php
@@ -338,8 +338,8 @@
                         
                         <div class="fh5co-text ">
                            
-                          <div style="color:#FBB448" class="col-xs-12 col-lg-12 col-md-12 col-sm-12">
-                            <h2 style="color:#FBB448" >{{$item['nomeproduto']}} {{$item['tags_adicionais']}}   {{ number_format($item['precoproduto'], 2) }}</h2>
+                          <div style="color:#FBB448" class="col-xs-12 col-lg-12 col-md-12 col-sm-12 img-thumbnail">
+                            <h2   style="color:#FBB448" >{{$item['nomeproduto']}} {{$item['tags_adicionais']}}   {{ number_format($item['precoproduto'], 2) }}</h2>
                           </div>
 
                           <div class="col-xs-12 col-lg-12 col-md-12 col-sm-12">
