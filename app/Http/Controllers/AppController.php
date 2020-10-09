089632594175fcde08ecbf17e0ad98059df9c1b4
Your Name <you@example.com>
2020-10-09 15:36:30 -0300

    'dwad

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -228,7 +228,7 @@ class AppController extends Controller
         foreach ($produtos as $key => $value) {
           $getxx =  new Produto();
           $getxx =  $getxx::verifica_tempo_promocao($getuser->codigo_estabelecimento, $value->id);
-           echo  "executando..";
+          echo  $getxx ;
           
            
         }
