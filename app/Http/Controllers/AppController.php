ef5aa841f6b52eb5147335512c22c04516ae380e
Your Name <you@example.com>
2020-10-09 14:30:59 -0300

    'wadawd

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -223,7 +223,7 @@ class AppController extends Controller
         //Realia o update das promocoes
         $getuser =  DB::table('users')->where('codigo_estabelecimento',$codeloja)->first();
         $produtos = DB::table('produtos')->where('ID_USER',  $getuser->id)->get();
-        
+        var_dump($produtos);
         foreach ($produtos as $key => $value) {
 
            $getxx =  \App\Produto::verifica_tempo_promocao($getuser->codigo_estabelecimento, $value->id);
