481e949a70dcb0799420162c8cb03355331fdbd1
Your Name <you@example.com>
2020-10-13 16:08:02 -0300

    'dwad

--- app/Http/Controllers/VendaController.php
+++ app/Http/Controllers/VendaController.php
@@ -701,7 +701,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://ws.pagseguro.uol.com.br/v2/checkout?email=gabrieldias@keemail.me&token=a6d998a5-b3a0-43c4-b1e2-84b69e347c52a97cd4b049dbb5325d003db5c6bc0bf7e77d-f4a8-4fa0-a4e2-1d1c3a890f0c');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "email=gabrieldias@keemail.me&token=a6d998a5-b3a0-43c4-b1e2-84b69e347c52a97cd4b049dbb5325d003db5c6bc0bf7e77d-f4a8-4fa0-a4e2-1d1c3a890f0c&currency=BRL&itemId1=001&itemDescription1=Items teste%201&itemAmount1=169.90&itemQuantity1=1&reference=124665c23f7896adff508377925&senderName=gabriel dias%20Green&senderAreaCode=51&senderPhone=988888888&senderEmail=emaildocomprador@pagseguro.com.br&shippingAddressRequired=true&extraAmount=0.00");
+curl_setopt($ch, CURLOPT_POSTFIELDS, "email=gabrieldias@keemail.me&token=a6d998a5-b3a0-43c4-b1e2-84b69e347c52a97cd4b049dbb5325d003db5c6bc0bf7e77d-f4a8-4fa0-a4e2-1d1c3a890f0c&currency=BRL&itemId1=001&itemDescription1=Items teste%201&itemAmount1=169.90&reference=124665c23f7896adff508377925&senderName=gabriel dias%20Green&senderAreaCode=51&senderPhone=988888888&senderEmail=emaildocomprador@pagseguro.com.br&shippingAddressRequired=true&extraAmount=0.00");
 
 $headers = array();
 $headers[] = 'Content-Type: application/x-www-form-urlencoded';
