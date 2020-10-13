9bb04d0d39c9b09d759a82e7e302410e09cabacc
Your Name <you@example.com>
2020-10-13 12:08:34 -0300

    'dwad

--- app/Http/Controllers/VendaController.php
+++ app/Http/Controllers/VendaController.php
@@ -28,7 +28,7 @@ use App\Http\Controllers\PagSeguroLibrary\PagSeguroLibrary ;
 
 
 
-require_once "../PagSeguroLibrary/PagSeguroLibrary.php";
+require_once "./PagSeguroLibrary/PagSeguroLibrary.php";
 
 
 class VendaController extends Controller
