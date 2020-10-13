993d869fc49723e712c911ebc9301456032b4c13
Your Name <you@example.com>
2020-10-13 16:43:05 -0300

    'dwad

--- app/Http/Controllers/PagseguroCheckout.php
+++ app/Http/Controllers/PagseguroCheckout.php
@@ -47,7 +47,7 @@ class PagseguroCheckout extends Controller
         curl_close($ch);
         var_dump($result);
              
-        $checkoutUrl = "https://pagseguro.uol.com.br/v2/checkout/payment.html?code=" .  $result;
+        $checkoutUrl = "https://pagseguro.uol.com.br/v2/checkout/payment.html?code=" . var_dump($result);
         return  $checkoutUrl;
     }
 
