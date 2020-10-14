22cd7550d732c7d8c5a29c460057b2726b726f26
Your Name <you@example.com>
2020-10-14 16:04:43 -0300

    'dwad

--- resources/views/Config.blade.php
+++ resources/views/Config.blade.php
@@ -322,6 +322,11 @@ visibility: hidden;
                                     <h3>Código Skytef</h3>
                                     <input type="text" class="form-control" name="redecode" id="redecode"  value="{{$usercf->redecode ?? ''}}">
                                 </div>
+
+                                <div class="col-xs-6 col-lg-6 col-md-6 col-sm-6">
+                                    <h3>Código Pagseguro</h3>
+                                    <input type="text" class="form-control" name="pagseguro" id="pagseguro"  value="{{$usercf->pagseguro ?? ''}}">
+                                </div>
                         </div>
                         <br>
                         <br>
