85f48412ea316350979153225ec886bc74f5b97d
Your Name <you@example.com>
2020-10-14 10:20:39 -0300

    'dwadaw

--- app/Http/Controllers/PagseguroCheckout.php
+++ app/Http/Controllers/PagseguroCheckout.php
@@ -150,9 +150,9 @@ class PagseguroCheckout extends Controller
     }
 
     public function set_valor_total($value){
-        $value = intval($value) * 10;   
-        $value = strval($value);
-        $value = intval($value);
+        //$value = intval($value) * 10;   
+      //  $value = strval($value);
+       // $value = intval($value);
         $this->valor_total = $value;
         return $this;
     }
