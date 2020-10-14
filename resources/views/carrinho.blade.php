c051decaa88b5327a49d8e817728e9b8a0188855
Your Name <you@example.com>
2020-10-14 15:37:43 -0300

    'dwad

--- resources/views/carrinho.blade.php
+++ resources/views/carrinho.blade.php
@@ -1630,8 +1630,8 @@ function atualiza_obj_user(){
       processandovenda()
 
       let telefone = apenasNumeros(obj_venda.telefone) 
-      telefone =  telefone.substr(1)
-      telefone =  telefone.substr(2)
+     
+     
       console.log(telefone)
       array_produtos_json = JSON.stringify(array_produtos_venda)
       obj_final.produtos =  array_produtos_json
