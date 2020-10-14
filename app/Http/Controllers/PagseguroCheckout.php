6a0186fb5ff3067e215106fbdb1ba938b457ecd5
Your Name <you@example.com>
2020-10-14 10:59:44 -0300

    'edadaw

--- app/Http/Controllers/PagseguroCheckout.php
+++ app/Http/Controllers/PagseguroCheckout.php
@@ -153,7 +153,7 @@ class PagseguroCheckout extends Controller
         
         $nombre_format_francais = number_format( $value, 2, '.', ' ');
         $nombre_format_francais =   $nombre_format_francais /10;
-
+        $nombre_format_francais = number_format( $value, 2, '.', ' ');
         $this->valor_total =  $nombre_format_francais  ;
         return $this;
     }
