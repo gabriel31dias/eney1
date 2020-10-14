e3ebe1e27bd2e2798bdd91f3bc16814dd2f8b945
Your Name <you@example.com>
2020-10-14 10:54:54 -0300

    'edadaw

--- app/Http/Controllers/PagseguroCheckout.php
+++ app/Http/Controllers/PagseguroCheckout.php
@@ -150,7 +150,7 @@ class PagseguroCheckout extends Controller
     }
 
     public function set_valor_total($value){
-        $value = floatval( $value ) ;
+      
         $nombre_format_francais = number_format( $value, 2, '.', ' ');
         $this->valor_total =  $nombre_format_francais  ;
         return $this;
