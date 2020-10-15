1dcfa8a7f59b6ac68b7d67c5188779e0c116b24e
Your Name <you@example.com>
2020-10-15 11:42:56 -0300

    'dwad

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,18 +18,18 @@
 
 <body style=" zoom: 80%;">
 	<div id="container">
-		<form  method="POST" action="{{ route('register') }}">
+		<form id="formregister" method="POST" onsubmit="return isValidForm()" action="{{ route('register') }}">
 			<fieldset>
 				<legend><i class='fas fa-user icon-user'></i></legend>
                 <label for="nome">Nome:</label>
                 @csrf
-				<input type="text" name="name"  placeholder="Ex: Lucas Gomes">
+				<input type="text" name="name" id="name"  placeholder="Ex: Lucas Gomes">
 				<label for="email">E-mail:</label>
-				<input type="email" name="email" placeholder="Ex: lucas@gmail.com">
+				<input type="email" name="email" id="email" placeholder="Ex: lucas@gmail.com">
 				<label for="nomeEmpresa">Nome Da Empresa:</label>
-				<input type="text" name="nome_estabelecimento" placeholder="Ex: InovaTTius Solutions">
-				<label for="nome">CNPJ Da Empresa:</label>
-				<input type="text" placeholder="Ex: 36.922.552/0001-02">
+				<input type="text" id="nome_estabelecimento" name="nome_estabelecimento" placeholder="Ex: InovaTTius Solutions">
+				<label for="cnpj">CNPJ Da Empresa:</label>
+				<input type="text" id="cnpj" name="cnpj" placeholder="Ex: 36.922.552/0001-02">
 				<label for="nome">Senha:</label>
 				<input type="password" name="password" placeholder="Informe uma senha">
 				<label for="nome">Confirme sua senha:</label>
@@ -41,5 +41,26 @@
 			</fieldset>
 		</form>
 	</div>
+	<script>
+
+		let name = document.getElementById("name").value;
+        let email = document.getElementById("email").value;
+		let nomeempresa = document.getElementById("nome_estabelecimento").value;
+		let cnpj = document.getElementById("cnpj").value;
+		let password =  document.getElementById("password").value;
+		let pass_vr = document.getElementById("password_confirmation").value;
+
+		document.getElementById('formregister').onsubmit = function() {
+
+              return isValidForm();
+        };
+
+		function isValidForm(){
+
+			return false
+		}
+
+
+	</script>
 </body>
 
