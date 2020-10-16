30c64ccde321e9c0425e914928ed5605a7141720
Your Name <you@example.com>
2020-10-16 15:49:24 -0300

    'ddwadwa

--- app/Http/Controllers/SwitchForma.php
+++ app/Http/Controllers/SwitchForma.php
@@ -58,7 +58,6 @@ class SwitchForma extends Controller
           ///https://ws.sandbox.pagseguro.uol.com.br/v3/transactions/0AB8B171-DFAA-4003-8C8E-0D1CE58C8A1A?email=gabrieldias@keemail.me&token=B401968342C944079D49933B107A188A
 
        $getemail_token = SwitchForma::getemailtoken($codevenda);//passa o token do pag seguro 
-       return $getemail_token;
        $url =  "https://ws.sandbox.pagseguro.uol.com.br/v3/transactions/".$codevenda.$getemail_token;
        $curl = curl_init($url);
        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
@@ -79,7 +78,6 @@ class SwitchForma extends Controller
         $getcalback_pg = DB::table('users')->where('codigo_estabelecimento', $_GET['loja'])->first();
         $email =  $getcalback_pg->emailpagseguro;
         $token = $getcalback_pg->pagsegurocode; 
-
         return "?email={$email}&token={$token}";
      /// return response()->json($getcalback_pg);
    }
