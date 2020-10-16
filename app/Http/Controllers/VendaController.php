0bd021e20bf4b8041a82c8bbd3f5b2dee0f82847
Your Name <you@example.com>
2020-10-16 15:26:39 -0300

    'ddwadwa

--- app/Http/Controllers/VendaController.php
+++ app/Http/Controllers/VendaController.php
@@ -535,6 +535,8 @@ class VendaController extends Controller
     public function mudastatus( Request $req){
         //Função responsavel por mudar status da tranzação
 
+        return $_GET['loja'];
+
         if(isset($_POST['notificationType']) && $_POST['notificationType'] == 'transaction'){
             ///Se for pagseguro entra aqui
              $response = SwitchForma::requestVenda($_POST['notificationCode']);
