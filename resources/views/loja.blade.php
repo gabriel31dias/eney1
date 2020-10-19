ee1c8684872ad5875ed5445f0aff37e560b772ea
Your Name <you@example.com>
2020-10-19 14:14:47 -0300

    'sedw

--- resources/views/loja.blade.php
+++ resources/views/loja.blade.php
@@ -732,7 +732,7 @@ if (formValues) {
  function addquantidade(){
 	 alert( parseFloat(preco_old))
 	 alert( parseFloat(total_adicionais) + parseFloat(total_adicionais) )
-	 let gr = ( parseFloat(preco_old) +  parseFloat(total_adicionais)  * cont_quantidade + 1)
+	 let gr = ( parseFloat(preco_old) +  parseFloat(total_adicionais)  ) * cont_quantidade + 1
 	   
 
 	  
