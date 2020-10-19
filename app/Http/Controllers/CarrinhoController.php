8562062b3cef8444d79427ef014688ffdba48dfa
Your Name <you@example.com>
2020-10-19 11:46:13 -0300

    'sedw

--- app/Http/Controllers/CarrinhoController.php
+++ app/Http/Controllers/CarrinhoController.php
@@ -187,6 +187,7 @@ class CarrinhoController extends Controller
 
         }
         $aux = Session::get('totaladicionais');
+        $getpreco_adicionais_orig = $getpreco_adicionais;
         $getpreco_adicionais = $getpreco_adicionais * $quantidade;
         Session::put('totaladicionais', $getpreco_adicionais + $aux); ///Salva valor total dos
         $idproduto_gerado =  rand(10, 500000);
@@ -213,7 +214,7 @@ class CarrinhoController extends Controller
          //Salva informações no array produtos savos que posteriormente sera salvo no cash do carrinho
         array_push($produtos_salvos, ['id' => $idproduto_gerado, 'codigoproduto'=>$getcodigoproduto , 'valorcusto'=> $getvalorcusto, 'cod_grupo_sistema'=> $getcod_grupo_SISTEMA , 'cod_adc_sistema'=>$array_cod_adicionais_sistema  ,
          'cfop'=> $getcfop, 'code_grupo_sitema'=> $get_cod_sist_grupo, 'idproduto' => $idproduto,
-         'nomeproduto' => $nomeproduto, 'precoproduto' =>    ($getpreco_adicionais +  $aux2)    , 'precoproduto2' => $getpreco  * $quantidade , 'valortotaladicionais' =>  $getpreco_adicionais, 'valortotal_produto_adicionais' =>  $aux2   +  $getpreco_adicionais,
+         'nomeproduto' => $nomeproduto, 'precoproduto' =>    ($getpreco_adicionais +  $aux2)    , 'precoproduto2' => $getpreco  * $quantidade , 'valortotaladicionais' =>  $getpreco_adicionais, 'valortotal_produto_adicionais' => $getpreco   +   $getpreco_adicionais_orig ,
          'img' => $getimg, 'adicionais'=>$adicionais, 'tags_adicionais'=>$tags_adicionais, 'obs'=>$obs, 'soproduto'=>$getpreco, //soproduto é o preço do produto sem adicionaç
         'precoadicionais' => $getpreco_adicionais * $quantidade, 'quantidade'=> $quantidade]);
 
