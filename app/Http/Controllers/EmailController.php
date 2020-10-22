4ea29c6d84641a7751cd3cd2a29a63ecc43a3368
Your Name <you@example.com>
2020-10-22 13:50:50 -0300

    'dwadwa

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmailController extends Controller
+{
+    
+    public function SendEmail($password,$message,$email){
+
+
+
+
+       // return response()->json($response);
+    }
+
+
+   
+
+
+}
---
+++ app/Http/Controllers/EmailValidController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\EmailController;
+use App\Validatestoken;
+use App\Smsenviado;
+
+class EmailValidController extends Controller
+{
+    
+    public function sendEmail($email,$codeloja){
+      $email = new EmailController();
+      $generatetoken = rand(1000,50000);
+      $newtoken = new Validatestoken();
+      $newtoken->token = $generatetoken;
+      $newtoken->save();
+      return   $email->SendEmail('0030015529','O seu token do WebApp é '.$generatetoken, $email);
+      $gg = $newtoken->all();
+   }
+
+   public function verificatoken($token){
+      $sms = new Validatestoken();
+      $sms = $sms->where('token',$token)->first();
+      
+      if(isset($sms->id)){
+        $sms->delete();
+          return   response()->json(['su'=>true]) ;
+      }else{
+      ////  $sms->delete();
+          return   response()->json(['su'=>false]) ;
+      }
+       
+   }
+
+}
