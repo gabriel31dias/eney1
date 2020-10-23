c1743a588dff1ac131525ff353d10adc26f9c45f
Your Name <you@example.com>
2020-10-23 15:43:01 -0300

    'dewad

--- resources/views/emails/test.blade.php
+++ resources/views/emails/test.blade.php
@@ -1,11 +1,15 @@
 
     <body>
-        <div  style="background-color: rgb(71, 58, 143)">
+       
+        <div  style="background-color: rgb(71, 58, 143);color:white">
+            <center>
         <h1>Olá {{ $user }}, tudo bem !</h2>
         <p>Este é um email do sistema de delivery da versatil</p>
         <p style="background-color: aqua">Seu token é <a style="color:red"> {{ $token }} </a></p>
         <p></p>
         <p>Att, <br>
         Sistema versatil Delivery !</p>
+    </center>
         </div>
+    
     </body>
