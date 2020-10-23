57c8062d875a3613f733b2088fe3885e2164a4bb
Your Name <you@example.com>
2020-10-23 10:55:03 -0300

    'dewad

--- resources/views/emails/test.blade.php
+++ resources/views/emails/test.blade.php
@@ -1,6 +1,6 @@
 
     <body>
-        <p>Olá !</p>
+        <p>Olá {{ $user }} bem vindo !</p>
         <p></p>
         <p>Esse é apenas um e-mail de teste, para exemplificar o funcionamento do envio de e-mails no Laravel.</p>
         <p></p>
