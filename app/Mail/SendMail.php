6b0d4500a444c5f1e2fe0fb76eb5c2bbc596f155
Your Name <you@example.com>
2020-10-23 10:19:00 -0300

    'dewad

--- app/Mail/SendMail.php
+++ app/Mail/SendMail.php
@@ -31,7 +31,7 @@ class SendMail extends Mailable
     public function build()
     {
         return $this->from('to@email.com')
-                ->view('emails.test')
+                ->view('emails.tedddst')
                 ->with([
                     'user' => $this->name,
                 ]);
