967f024f19adc6e35581e2284837fcb6dac44d29
Your Name <you@example.com>
2020-10-26 11:58:45 -0300

    'dewad

--- database/migrations/2020_10_26_115748_add_idusermetrics.php
+++ database/migrations/2020_10_26_115748_add_idusermetrics.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMetricsTable extends Migration
+class AddIdusermetrics extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateMetricsTable extends Migration
      */
     public function up()
     {
-        Schema::create('metrics', function (Blueprint $table) {
-            $table->id();
-            $table->integer('totalaccess')->nullable();
-            $table->string('totallikes')->nullable();
-           
-            $table->timestamps();
+        Schema::table('metrics' , function(Blueprint $table){
+            $table->integer('ID_USER');
         });
     }
 
@@ -29,6 +25,6 @@ class CreateMetricsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('metrics');
+        //
     }
 }
