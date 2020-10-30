ccc11f806b4afb09bd9538f50cd6b6330afe5881
Your Name <you@example.com>
2020-10-30 09:27:46 -0300

    fesfse

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -172,7 +172,7 @@
           <div class="col-md-5">
             <div class="row rotacionar">
               <div class="col-md-6">
-                <img src="imagens/iphone1.png" class="img-fluid">
+                <img src="/site/imagens/app.jpeg" class="img-fluid">
               </div>
             </div>
           </div>
